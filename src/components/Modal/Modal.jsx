4c07f5dc1aab2e0c74b70e658d8b0cf635c6c6ca
kirmann <k.dkle@hotmail.fr>
2022-04-20 13:49:51 +0200

    implémentation et style de la modale

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export const Modal = () => {
+export const Modal = (props) => {
+  let { status, message, toggleModal } = props
+
+
   return (
-    <div>
-      <h1>test</h1>
+    <div className={status ? "openModal" : "closeModal"}>
+      <div className="modalContainer">
+        <p>{message}</p>
+        <button onClick={() => toggleModal()}>X</button>
+      </div>
     </div>
-  );
+  )
 };
 
