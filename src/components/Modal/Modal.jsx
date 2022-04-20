f71afda568b2c11cb44b76c4ac569a5df6909c27
kirmann <k.dkle@hotmail.fr>
2022-04-20 14:02:46 +0200

    import du style dans modale.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import './modal.css'
 
 export const Modal = (props) => {
   let { status, message, toggleModal } = props
-
 
   return (
     <div className={status ? "openModal" : "closeModal"}>
