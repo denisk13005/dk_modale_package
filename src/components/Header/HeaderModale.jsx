49fb578cc504424155cf7c127e4c8578ac072a3d
kirmann <k.dkle@hotmail.fr>
2022-04-23 11:36:09 +0200

    ajout d'un header et d'un footer a la modale

---
+++ src/components/Header/HeaderModale.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderModale = () => {
+  return (
+    <div>
+        <h1>header</h1>
+    </div>
+  );
+};
+
+export default HeaderModale;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import FooterModale from '../FooterModale/FooterModale';
+import HeaderModale from '../Header/HeaderModale';
 import './modal.css'
 
 export const Modal = (props) => {
@@ -6,10 +8,13 @@ export const Modal = (props) => {
   
   return (
     <div className={status ? "openModal" : "closeModal"}>
+      <HeaderModale/>
+
       <div className="modalContainer">
         <p>{message}</p>
         <button onClick={() => toggleModal()}>X</button>
       </div>
+      <FooterModale/>
     </div>
   )
 };
